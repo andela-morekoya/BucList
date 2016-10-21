3b20f26a2242e7266f274966e74f3a7bb2da3246
Morolake Orekoya <morolake.orekoya@andela.com>
2016-10-21 15:32:15 +0100

    write user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+<<<<<<< HEAD
+=======
+  describe "Validations" do
+    subject { FactoryGirl.build(:user) }
+
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to allow_value("email@valid.com").for(:email) }
+    it { is_expected.to_not allow_value("email@invalid").for(:email) }
+
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  # describe "Associations" do
+  #   it { is_expected.to have_many(:links) }
+  #   it { is_expected.to have_many(:visits) }
+  # end
+>>>>>>> write user model tests
 end
