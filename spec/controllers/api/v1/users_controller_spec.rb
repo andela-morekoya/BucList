c90e28d846c3bf3e70d47d25e70c20704d439d75
Morolake Orekoya <morolake.orekoya@andela.com>
2016-10-26 10:18:18 +0100

    write test for user#create

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,19 +3,41 @@ require "rails_helper"
 RSpec.describe Api::V1::UsersController, type: :controller do
   before { request.headers['Accept'] = "application/vnd.secchio.v1" }
 
-  describe "GET #show" do
+  describe "#show" do
     let(:user) { FactoryGirl.create :user }
 
     before do
       get :show, params: { id: user.id }
     end
 
-    it "returns the information about a reporter on a hash" do
+    it "returns the specified user's information on a hash" do
       user_response = JSON.parse(response.body)
       expect(user_response["email"]).to eql user.email
     end
 
     it { is_expected.to respond_with 200 }
+  end
+
+  describe "#create" do
+    context "with valid params" do
+      it "responds with 201 - resource created" do
+        post :create, params: { user: FactoryGirl.attributes_for(:user) }
+        is_expected.to respond_with 201
+      end
+    end
+
+    context "with invalid params" do
+      before { post :create, params: { email: "invalid" } }
+      it "renders error saying why resource could not created" do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key(:errors)
+        expect(user_response[:errors]).to be "ladida"
+      end
+
+      it "responds with 422 - Unprocessable entity" do
+        is_expected.to respond_with 422
+      end
+    end
   end
 end
 # curl -H 'Accept: application/vnd.secchio.v1' http://localhost:3000/users/1
