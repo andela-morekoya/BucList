2f3bde2cd59415579d44b2208b561e49215ff97e
Morolake Orekoya <morolake.orekoya@andela.com>
2016-10-29 01:51:25 +0100

    cherry pick token implementatin from another branch

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
-  it { is_expected.to belong_to(:user).dependent(:destroy) }
-  it { is_expected.to validate_presence_of :token }
-  it { is_expected.to validate_uniqueness_of :token }
+  describe "Associations" do
+    it { is_expected.to belong_to :user }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :token }
+    it { is_expected.to have_uniqueness }
 
   describe "#is_valid" do
     let!(:user) { FactoryGirl.create(:user) }
