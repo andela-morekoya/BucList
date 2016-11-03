506050a6ee4000399c0245c1292523c1d297abea
Morolake Orekoya <morolake.orekoya@andela.com>
2016-11-03 02:24:06 +0100

    add test to check token association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,6 +35,36 @@ RSpec.describe User, type: :model do
 =======
   describe "Associations" do
     it { is_expected.to have_many(:bucketlists) }
+    it { is_expected.to have_one(:token) }
+  end
+
+  describe ".generate_token" do
+    let (:user) { FactoryGirl.create(:user) }
+
+    context "when token does not exist" do
+      it "gives user a new token" do
+        user.generate_token
+        expect(user.token).to be_present
+      end
+    end
+
+    context "when token already exists" do
+      before { user.generate_token }
+      context "and token has expired" do
+        it "returns nil" do
+          user.token.update(expires_at: 3.hours.ago)
+          user.generate_token
+          expect(user.token).to be nil
+        end
+      end
+
+      context "and token has not expired" do
+        it "returns the user's token" do
+          result = user.generate_token
+          expect(user.token).to eq result
+        end
+      end
+    end
   end
 >>>>>>> fix rubocop issues
 end
