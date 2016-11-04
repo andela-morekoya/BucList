97863bea56b2c3c5f8847fb8190bc95b92b99d78
Morolake Orekoya <morolake.orekoya@andela.com>
2016-11-04 11:29:57 +0100

    add token table migration

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ group :development do
   gem 'web-console'
   gem 'listen', '~> 3.0.5'
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-watcher-listen'
 end
 
 group :production do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102141052) do
+ActiveRecord::Schema.define(version: 20161104101150) do
 
   create_table "bucketlists", force: :cascade do |t|
     t.string   "name"
