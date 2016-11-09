45660ad1e9da4a3a3b557b76bc72aab964074dbf
Morolake Orekoya <morolake.orekoya@andela.com>
2016-11-09 15:07:40 +0100

    refactor bucketlist model and controller

--- app/controllers/api/v1/bucketlists_controller.rb
+++ app/controllers/api/v1/bucketlists_controller.rb
@@ -2,59 +2,64 @@ module Api
   module V1
     class BucketlistsController < ApplicationController
       include Messages
-      before_action :set_bucketlist, only: [:show, :update, :destroy]
       before_action :authenticate_request
-
+      before_action :set_bucketlist, only: [:show, :update, :destroy]
+      
       def index
-        @bucketlists = Bucketlist.search_and_paginate(params)
+        @bucketlists = (current_user.bucketlists.search(params[:q]) \
+          if params[:q]) || current_user.bucketlists.all
+
+        if params[:page] || params[:limit]
+          @bucketlists = @bucketlists.paginate(params[:limit], params[:page]) 
+        end
+
         render json: { bucketlists: @bucketlists }, status: :ok
       end
 
       def show
-        render json: @bucketlist, status: :ok
+        render json: { bucketlist: @bucketlist }, status: :ok
       end
 
       def create
         @bucketlist = current_user.bucketlists.build(bucketlist_params)
 
         if @bucketlist.save
-          render json: @bucketlist, status: :created
+          render json: { bucketlist: @bucketlist }, status: :created
         else
-          render json: { error: not_created('Bucketlist') }, \
+          render json: { errors: not_created('Bucketlist') }, \
                  status: :bad_request
         end
       end
 
       def update
-        if @bucketlist.user_id == current_user.id
-          if @bucketlist.update(bucketlist_params)
-            render json: @bucketlist, status: :ok
-          else
-            render json: { error: not_updated('Bucketlist') }, \
-                   status: :bad_request
-          end
+        if @bucketlist.update(bucketlist_params)
+          render json: { bucketlist: @bucketlist }, status: :ok
         else
-          render json: { error: no_access }, status: :forbidden
+          render json: { errors: not_updated('Bucketlist') }, \
+                 status: :bad_request
         end
       end
 
       def destroy
-        if @bucketlist.user_id == current_user.id
-          @bucketlist.destroy
-          head 204
-        else
-          render json: { error: no_access }, status: :forbidden
-        end
+        @bucketlist.destroy
+        head 204
       end
 
       private
 
       def set_bucketlist
-        @bucketlist = Bucketlist.find(params[:id])
+        @bucketlist = Bucketlist.find_by(id: params[:id])
+        authorize_user
       end
 
       def bucketlist_params
         params.permit(:name)
+      end
+
+      def authorize_user
+        unless current_user.id == @bucketlist.user_id 
+          render json: { errors: no_access }, status: :forbidden  
+        end
       end
     end
   end
