0dbfc21e78dc78684366b9ead6c67ab54701e5f0
Morolake Orekoya <morolake.orekoya@andela.com>
2016-11-14 10:42:17 +0100

    remove forgotten code

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,10 @@ Rails.application.routes.draw do
   get "/", to: redirect("/docs/index.html")
 
   namespace :api, defaults: { format: :json } do
-    scope module: :v2, \
-          constraints: ApiConstraints.new(version: 2) do
-
-    end
-
-    scope module: :v1, \
+    scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
       post "/auth/login"    => "auth#login"
-      delete "/auth/logout"    => "auth#logout"
+      delete "/auth/logout" => "auth#logout"
       resources :users, only: [:create]
       resources :bucketlists do
         resources :items
